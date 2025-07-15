51925cbc8cc74da264b245e9cf284130724404b6
guancome4 <guancome4@gmail.com>
2025-07-15 13:37:00 -0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-const supabaseUrl = 'https://tu-proyecto.supabase.co'
-const supabaseKey = 'tu-clave-publica'
+const supabaseUrl = 'https://vdjsgllncfekshbdvqpw.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkanNnbGxuY2Zla3NoYmR2cXB3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1OTUzODIsImV4cCI6MjA2ODE3MTM4Mn0.W8J9be_dE_aqc5ES7eurInCpl8MEbc7KlQZj3bLs3Fs'
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
 // Función para obtener el usuario actual
